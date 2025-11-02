f0dfc194c4f0af8c3d335553d24da0f60604b337
blattgold <ashlig@protonmail.com>
2025-11-02 14:25:27 +0100

    TRY_CONVERT

--- ruct_tests.c
+++ ruct_tests.c
@@ -5,17 +5,24 @@ Ruct_Result_None test_try_fails_helper() {
 }
 RUCT_TEST_DEFINE(test_try_fails,
     Ruct_None n = RUCT_TRY(test_try_fails_helper());
-    return RUCT_OK_NONE;
 )
 
 Ruct_Result_None test_try_succeeds_helper() {
     return RUCT_OK_NONE;
-};
+}
 RUCT_TEST_DEFINE(test_try_succeeds,
     Ruct_None n = RUCT_TRY(test_try_succeeds_helper());
+)
+
+Ruct_Result_U32 test_try_convert_fails_helper() {
+    return Ruct_Err_U32("failed successfully.");
+}
+RUCT_TEST_DEFINE(test_try_convert_fails,
+    u32 v = RUCT_TRY_CONVERT(test_try_convert_fails_helper(), Ruct_Err_None);
 )
 
 RUCT_TEST_MODULE( "ruct_tests",
     RUCT_TEST_FAIL(test_try_fails);
     RUCT_TEST(test_try_succeeds);
+    RUCT_TEST_FAIL(test_try_convert_fails);
 )
